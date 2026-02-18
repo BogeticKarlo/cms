8f660fdcb7af20140ec53beeb8d95454974379c8
BogeticKarlo <147347211+BogeticKarlo@users.noreply.github.com>
2026-02-18 21:28:09 +0100

    modified importMap.js

--- cms/src/app/(payload)/admin/importMap.js
+++ cms/src/app/(payload)/admin/importMap.js
@@ -1,3 +1,4 @@
+"use client"
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
