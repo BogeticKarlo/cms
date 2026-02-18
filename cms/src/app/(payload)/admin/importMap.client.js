4fbcaa2c45648e0aafd7b3298553589cf6084f93
BogeticKarlo <147347211+BogeticKarlo@users.noreply.github.com>
2026-02-18 21:37:23 +0100

    making a workaround

--- cms/src/app/(payload)/admin/importMap.client.js
+++ cms/src/app/(payload)/admin/importMap.client.js
@@ -1,8 +1,12 @@
 "use client"
-import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
-import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
-export const importMap = {
-  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
-  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+export async function getImportMap() {
+  const [{ S3ClientUploadHandler }, { CollectionCards }] = await Promise.all([
+    import('@payloadcms/storage-s3/client'),
+    import('@payloadcms/next/rsc'),
+  ]);
+  return {
+    "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler,
+    "@payloadcms/next/rsc#CollectionCards": CollectionCards,
+  };
 }
--- cms/src/app/(payload)/layout.tsx
+++ cms/src/app/(payload)/layout.tsx
@@ -5,28 +5,34 @@ import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import { UploadHandlersProvider } from '@payloadcms/ui'
 import React from 'react'
 
-import { importMap } from './admin/importMap.client.js'
+import { getImportMap } from './admin/importMap.client.js'
 import './custom.scss'
 
 type Args = {
   children: React.ReactNode
 }
 
+
 const serverFunction: ServerFunctionClient = async function (args) {
   'use server'
+  const importMap = await getImportMap();
   return handleServerFunctions({
     ...args,
     config,
     importMap,
-  })
+  });
 }
 
-const Layout = ({ children }: Args) => (
-  <UploadHandlersProvider>
-    <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
-      {children}
-    </RootLayout>
-  </UploadHandlersProvider>
-)
+
+const Layout = async ({ children }: Args) => {
+  const importMap = await getImportMap();
+  return (
+    <UploadHandlersProvider>
+      <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+        {children}
+      </RootLayout>
+    </UploadHandlersProvider>
+  );
+}
 
 export default Layout
